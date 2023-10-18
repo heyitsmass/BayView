8d057d90c14b59d0d46e3d4068abd2e97cf838fe
Joshua Sahagun <sahagj1@unlv.edu>
2023-10-18 12:25:36 -0700

    added skeleton for the /home/guest page

---
+++ src/app/home/guest/page.tsx
@@ -0,0 +1,11 @@
+import styles from "./page.module.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+export default async function Page({children}:{children:react.ReactNode}){
+
+    return (
+    <div>
+        Guest Page
+    </div>
+    )
+}
