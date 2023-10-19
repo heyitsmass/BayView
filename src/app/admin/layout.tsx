3b29d84c33d792e66eadd11209e3d31086e510fc
Salvador Rangel <salvadorrangel29@gmail.com>
2023-10-19 11:38:43 -0700

    made new directory for the admin and login directory for login page. implemented admin login page and styles.

---
+++ src/app/admin/layout.tsx
@@ -0,0 +1,7 @@
+import { ReactNode } from 'react';
+import './layout.css'; 
+
+export default async function Layout({children}:{children:any}){ 
+
+    return <div>{children}</div>
+}
---
+++ src/app/admin/login/page.tsx
@@ -0,0 +1,25 @@
+import Link from "next/link";
+import UserInput from "@/app/home/login/components/Input";
+import styles from './page.module.css';
+import { faUser, faKey } from '@fortawesome/free-solid-svg-icons'
+
+
+
+export default async function Page({children}:{children:any}){ 
+
+    return <div className={styles.login}>
+        
+        <div className="admin-login">
+
+        <h1>BayView </h1>
+        <h1>Admin Login</h1>
+        <form>
+            <UserInput inlineIcon={{ icon: faUser }} type="text" placeholder="Username" required />
+            <UserInput inlineIcon={{ icon: faKey }} type="password" placeholder="Password" required />
+            <button type="submit">Login</button>
+        </form>
+
+        </div>
+
+    </div>;
+}
