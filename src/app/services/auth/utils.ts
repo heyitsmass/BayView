3b8149213d449818b97cb9393b23be77047434cc
Brandon C. <brandon.cannon88@gmail.com>
2023-10-19 19:49:17 -0700

    Created Auth Cred. Gen. - Matches Google Schema

--- src/app/services/auth/utils.ts
+++ src/app/services/auth/utils.ts
@@ -22,3 +22,43 @@ export async function updateCookies({ ...args }: Credentials) {
 export const createToken = (payload: any, expiresIn: number | string) =>
   jwt.sign(payload, process.env.TOKEN_KEY!, { expiresIn });
 
+export const getCredentials = (username: string, _id?: string) => {
+  if (!_id) {
+    const user_id = new mongoose.Types.ObjectId();
+    _id = user_id.toString();
+  }
+
+  const maxAge = 2 * 60 * 1000; //in milliseconds
+  //console.log("Access Token: ", { _id });
+
+  const access_token = createToken({ _id }, `${maxAge}`);
+  const refresh_token = createToken(
+    {
+      username,
+      access_token
+    },
+    maxAge
+  );
+  //console.log("Access Token: ", { username, access_token });
+  const id_token = createToken(
+    {
+      _id,
+      username,
+      access_token,
+      refresh_token
+    },
+    maxAge
+  );
+  //console.log("Id Token: ", { username, access_token });
+
+  return {
+    _id,
+    credentials: {
+      access_token,
+      refresh_token,
+      expiry_date: Date.now() + maxAge,
+      id_token,
+      scope: SCOPES.USER.default.join(" ")
+    } as Credentials
+  };
+};
