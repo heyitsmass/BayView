eb3748455676d1532632fc753c831f1991b8154b
Brandon C. <brandon.cannon88@gmail.com>
2023-10-19 19:05:12 -0700

    Removed unused implementation

--- next.config.mjs
+++ next.config.mjs
@@ -1,27 +1,4 @@
-/** @type {import('next').NextConfig} */
-import { merge } from "webpack-merge";
-
-const config = (fb, { defaultConfig }) => {
-  return {
-    webpack: (cfg, { isServer, ...ctx }) => {
-      if (isServer) {
-        return merge(cfg, {
-          module: {
-            rules: [
-              {
-                test: /\.node$/,
-                loader: "node-loader"
-              }
-            ]
-          },
-          node: {
-            __dirname: false
-          }
-        });
-      }
-      return cfg;
-    },
-    redirects: async () => [
+/**
       {
         source: "/",
         permanent: true,
