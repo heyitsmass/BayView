aecc824df74adf078bc065f059c29816fd7fbf8b
Brandon C. <brandon.cannon88@gmail.com>
2023-10-19 19:47:53 -0700

    Created Cookie management function

---
+++ src/app/services/auth/utils.ts
@@ -0,0 +1,20 @@
+"use server";
+
+import { Credentials } from "google-auth-library";
+import mongoose from "mongoose";
+import { cookies } from "next/headers";
+
+import { SCOPES } from "../constants";
+import jwt from "jsonwebtoken";
+
+export async function updateCookies({ ...args }: Credentials) {
+  const cookieStore = cookies();
+
+  const { access_token, refresh_token, expiry_date } = args;
+
+  if (!access_token || !refresh_token || !expiry_date)
+    throw Error("Access token not found");
+
+  cookieStore.set("access_token", access_token, { expires: expiry_date });
+  cookieStore.set("refresh_token", refresh_token, { expires: expiry_date });
+}
