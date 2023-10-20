c4cd3b63591ae38e16eca3a1c50600959cc1f82d
Brandon C. <brandon.cannon88@gmail.com>
2023-10-19 19:46:17 -0700

    Implemented GoogleID creation methods

--- src/app/services/auth/google.ts
+++ src/app/services/auth/google.ts
@@ -24,6 +24,29 @@ export async function refreshTokens(refreshToken: string) {
     credentials
   };
 }
+const MONGO_DB_ID_LENGTH = 24;
+
+function hashCode(str: string) {
+  let hash = 0;
+  for (let i = 0, len = str.length; i < len; i++) {
+    let chr = str.charCodeAt(i);
+    hash = (hash << 5) - hash + chr;
+    hash |= 0; // Convert to 32bit integer
+  }
+  return Math.abs(hash);
+}
+
+const getIdFromGoogle = (googleId: string, email: string) => {
+  const _id = parseInt(googleId).toString(16);
+
+  return (
+    _id +
+    hashCode(email)
+      .toString(16)
+      .substring(0, MONGO_DB_ID_LENGTH - _id.length)
+  ); //new mongoose.Types.ObjectId(_id);
+};
+
 export default async function handleLogin(code: string) {
   const { tokens } = await oAuth2Client.getToken(code);
   await updateCookies(tokens);
