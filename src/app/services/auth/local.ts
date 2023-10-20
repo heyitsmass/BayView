e5ab1be582b071cd708344c8de2eee2b26347adc
Brandon C. <brandon.cannon88@gmail.com>
2023-10-19 19:50:05 -0700

    Created local auth Backend registration function

--- src/app/services/auth/local.ts
+++ src/app/services/auth/local.ts
@@ -15,3 +15,35 @@ const securePassword = async (password: string) => {
   return bcrypt.hash(password, salt);
 };
 
+export const registerUser = async (form: FormData) => {
+  const username = form.get("username") as string;
+  let user = await Users.findOne({
+    username
+  });
+
+  if (user) throw Error("Username already taken. Please login");
+
+  const email = form.get("email") as string;
+
+  const phone = form.get("phone") as string;
+  const name = {
+    first: form.get("first-name") as string,
+    last: form.get("last-name") as string
+  };
+
+  const password = await securePassword(form.get("password") as string);
+
+  const { _id, credentials } = getCredentials(username);
+
+  user = await Users.create({
+    authType: "email",
+    _id,
+    username,
+    email,
+    phone,
+    name,
+    password,
+    credentials
+  });
+};
+
