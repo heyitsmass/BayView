42a31bbc316dc619479a8ed5d800e795511c78bf
Chappy122 <97774131+Chappy122@users.noreply.github.com>
2023-10-23 09:56:04 -0700

    Update page.tsx
    Updated user input folder location

--- src/app/admin/login/page.tsx
+++ src/app/admin/login/page.tsx
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import UserInput from "@/app/home/login/components/Input";
+import UserInput from "@/components/Input";
 import styles from './page.module.css';
 import { faUser, faKey } from '@fortawesome/free-solid-svg-icons'
 
