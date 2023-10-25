a644521efc580cb79c6eb4e23b555fba8e443e14
Brandon <58617118+heyitsmass@users.noreply.github.com>
2023-10-25 05:45:47 -0700

    Created API Login method for authentication

--- src/api/lib/app.py
+++ src/api/lib/app.py
@@ -44,3 +44,62 @@ class WebDriver(Firefox):
 
         self.get(f"https://{host}.com/")  # get the host (cookie averse documents.)
 
+    def login(self, __max_retries=3):
+        """
+        Logs into the host website.
+
+        Args:
+            __max_retries (int, optional): The maximum number of retries to attempt. Defaults to 3.
+        """
+        self.get(f"https://{self.host}.com/login/")
+        frame = self.find_element(By.XPATH, '//*[@id="oneid-iframe"]')
+        self.switch_to.frame(frame)  # switch to the login frame
+
+        self.find_element(By.XPATH, '//*[@id="InputIdentityFlowValue"]').send_keys(
+            os.getenv("DISNEY_USERNAME")
+        )  # populate the email
+
+        self.find_element(
+            By.XPATH, '//*[@id="BtnSubmit"]'
+        ).click()  # click the submit button
+
+        WebDriverWait(self, 10).until(
+            EC.visibility_of_element_located(
+                (
+                    By.XPATH,
+                    '//*[@id="InputPassword"]',
+                )
+            )
+        )  # wait for the element to load
+
+        self.find_element(By.XPATH, '//*[@id="InputPassword"]').send_keys(
+            os.getenv("DISNEY_PASSWORD")
+        )  # populate the password field
+
+        self.find_element(
+            By.XPATH, '//*[@id="BtnSubmit"]'
+        ).click()  # click the login button
+        self.switch_to.default_content()  # switch to the page context
+
+        WebDriverWait(self, 10).until(
+            EC.visibility_of_element_located(
+                (
+                    By.XPATH,
+                    "/html/body/wdpr-ui-universal-layout/div[7]/div/div[2]/div[1]/div/div[1]/div/div[1]/div[2]/div[1]/a",
+                )
+            )
+        )  # wait until the login has completed.
+
+        self.auth = self.auth_status()  # collect the auth status
+
+        if not self.auth.isLoggedIn:
+            self.delete_all_cookies()
+            self.refresh()
+            if __max_retries > 0:  # retry if we failed (up to `max_retries` time)
+                print(f"Login failed. Retrying... {4 - __max_retries}")
+                self.login(__max_retries - 1)
+            else:
+                raise Exception("Login failed.")  # login failed
+
+        self.load_pre_conditions()  # load the dining res pre-conditions
+
