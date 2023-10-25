cc9e5295df71309cf04a07ac8d3bf1a11c868b11
Brandon <58617118+heyitsmass@users.noreply.github.com>
2023-10-25 05:42:58 -0700

    Created Profile response type

--- src/api/lib/abc.py
+++ src/api/lib/abc.py
@@ -34,3 +34,81 @@ class AccessToken(NamedTuple):
     expires_in: int
 
 
+"https://api.wdprapps.disney.com/pep/profile?isUser=True&brand=wdw&locale=en-us&userType=GUEST"
+
+"""
+    Avatar data returned from the endpoint.
+"""
+
+
+class Avatar(NamedTuple):
+    id: str
+    title: str
+    url: str
+
+
+"""
+    Self media links returned from the endpoint.
+"""
+
+
+class SelfLinks(NamedTuple):
+    href: str
+
+
+"""
+    Links returned from the endpoint.
+"""
+
+
+class Links(NamedTuple):
+    self: SelfLinks
+
+
+"""
+    Notification response returned from the endpoint.
+"""
+
+
+class NotificationResponse(NamedTuple):
+    startItem: int
+    itemLimit: int
+    itemCount: str
+    receivedInvitations: list[str]
+    links: Links
+
+
+"""
+    Guest identifier returned from the endpoint.
+"""
+
+
+class GuestIdentifier(NamedTuple):
+    type: str
+    value: str
+
+
+"""
+    User data returned from the endpoint.
+"""
+
+
+class User(NamedTuple):
+    avatar: Avatar
+    notificationResponse: NotificationResponse
+    firstName: str
+    lastName: str
+    email: str
+    guestIdentifiers: list[GuestIdentifier]
+    affiliations: list[str]
+    messageBanner: str
+    asyncMessagingToken: None | str = None
+
+
+"""
+    Profile returned from the endpoint.
+"""
+
+
+class Profile(NamedTuple):
+    user: User
