f51873a97e82938c8b48b6e7402207f456b2a4fc
Brandon <58617118+heyitsmass@users.noreply.github.com>
2023-10-25 05:42:26 -0700

    Created auth response type

--- src/api/lib/abc.py
+++ src/api/lib/abc.py
@@ -1,2 +1,22 @@
 from typing import NamedTuple
 
+"https://disneyworld.disney.go.com/authentication/status/"
+
+"""
+    CSRF data returned from the endpoint.
+"""
+
+class CSRF(NamedTuple):
+    token: str
+    header: str
+
+"""
+    Authentication status returned from the endpoint.
+"""
+
+class AuthenticationStatus(NamedTuple):
+    csrf: CSRF
+    isLoggedIn: bool
+    isSecure: bool
+    swid: None | str = None
+
