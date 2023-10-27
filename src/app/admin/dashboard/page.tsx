2923717ff1b266f5a64fc7c2fb178f1262cf4055
Brandon C <58617118+heyitsmass@users.noreply.github.com>
2023-10-27 15:19:37 -0700

    Update page.tsx
    client components cannot be asynchronous

--- src/app/admin/dashboard/page.tsx
+++ src/app/admin/dashboard/page.tsx
@@ -1,11 +1,4 @@
-'use client'
-import Link from "next/link";
-import styles from './page.module.css';
-import { faUser, faKey } from '@fortawesome/free-solid-svg-icons'
-
-
-
-export default async function Page(){ 
+export default function Page(){ 
 
     return <>
     </>
