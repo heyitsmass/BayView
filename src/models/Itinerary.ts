2f7534136ee42640eda118628f717e61d97707eb
Brandon C <58617118+heyitsmass@users.noreply.github.com>
2023-10-27 16:12:56 -0700

    Update Itinerary.ts
    Necessary conditional change

--- src/models/Itinerary.ts
+++ src/models/Itinerary.ts
@@ -2,13 +2,13 @@ import mongoose, { Schema } from "mongoose";
 import { eventSchema } from "./Event";
 
 export const itinerarySchema = new Schema<IItinerary>({
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
+  startDate: { type: Date, required: false },
+  endDate: { type: Date, required: false},
   events: [eventSchema]
 });
 
 const Itineraries =
-  mongoose.models.Itinerary ||
+  mongoose.models?.Itinerary ||
   mongoose.model<IItinerary>("Itinerary", itinerarySchema);
 
 export default Itineraries;
