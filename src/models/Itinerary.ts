3a111b7e41c56582acd77caabc76cee05d01cc77
Joshua Sahagun <sahagj1@unlv.edu>
2023-10-27 16:10:52 -0700

    added .pre() to schema, adjusting a saved Itinerary as needed

--- src/models/Itinerary.ts
+++ src/models/Itinerary.ts
@@ -2,13 +2,26 @@ import mongoose, { Schema } from "mongoose";
 import { eventSchema } from "./Event";
 
 export const itinerarySchema = new Schema<IItinerary>({
-  startDate: { type: Date, required: true },
-  endDate: { type: Date, required: true },
+  startDate: { type: Date, required: true, default: Date.now() },
+  endDate: { type: Date, required: true, default: Date.now() },
   events: [eventSchema]
 });
 
 const Itineraries =
-  mongoose.models.Itinerary ||
-  mongoose.model<IItinerary>("Itinerary", itinerarySchema);
+    mongoose.models.Itinerary ||
+    mongoose.model<IItinerary>("Itinerary", itinerarySchema);
+
+itinerarySchema.pre('save', (next) => {
+  // assuming that elements in events are sorted
+  //    in chronological order
+  console.log("checking if event length is populated")
+  if(this.events.length > 0){
+    console.log("events found")
+    this.startDate = events[0].date;
+    this.endDate = events[events.length - 1].date;
+  }
+  console.log("returning next")
+  next();
+});
 
 export default Itineraries;
