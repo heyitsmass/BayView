f8fe9b99c11eb21a3f9b30d1aebda70493dcc52a
Brandon C <58617118+heyitsmass@users.noreply.github.com>
2023-10-28 19:30:47 -0700

    Update index.tsx
    - [Removed default export](https://medium.com/@islizeqiang/why-you-should-avoid-default-exports-in-javascript-modules-7d087dde7ac6#:~:text=One%20of%20the%20main%20problems,because%20they%20are%20not%20named.)

--- src/components/Input/InputPair/index.tsx
+++ src/components/Input/InputPair/index.tsx
@@ -2,7 +2,7 @@
 
 import { ReactNode } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { IconDefinition, library, icon } from "@fortawesome/fontawesome-svg-core";
+import { IconDefinition } from "@fortawesome/fontawesome-svg-core";
 import styles from "./inputpair.module.css";
 
 type InputPairProps = {
@@ -11,19 +11,17 @@ type InputPairProps = {
 	children: [ReactNode, ReactNode];
 };
 
-export default function InputPair({ className, ...props }: InputPairProps) {
+export function InputPair({ ...props }: InputPairProps) {
 	
-	// If icon exists add to libary
-    if(props.icon)
-		library.add(props.icon);
-
+	const {className, icon, children} = props; 
+	
 	return (
 		<div className={`${className} flex items-center justify-between w-100`}>
-			<div className="w-full">{props.children[0]}</div>
+			<div className="w-full">{children[0]}</div>
 			<div className={styles.inlineIcon}>
-				{props.icon ? <FontAwesomeIcon className="dark:text-white text-black w-3" size="lg" icon={icon(props.icon)} /> : <div className="w-3"></div>}
+				{icon ? <FontAwesomeIcon className="dark:text-white text-black w-3" size="lg" icon={icon} /> : <div className="w-3"></div>}
 			</div>
-			<div className="w-full">{props.children[1]}</div>
+			<div className="w-full">{children[1]}</div>
 		</div>
 	);
 }
