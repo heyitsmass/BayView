9df6740be67dc0a3e6390cc16e13ed5f288b7f9b
cpilande <120698868+cpilande@users.noreply.github.com>
2023-11-02 12:30:09 -0700

    Itinerary Event

---
+++ src/components/ItineraryEvent/index.tsx
@@ -0,0 +1,16 @@
+import React, { ReactNode, useState, HTMLProps } from "react";
+import styles from "./itineraryevent.module.css";
+
+export default function ItineraryEvent({ time, location, guestNumber }) {
+  return (
+    <div className="inline-block">
+      <div className={styles.time}>
+        <p className="ml-6"> {time} AM </p>
+      </div>
+      <div className={styles.info}>
+        <p className="inline"> {location} Reservation | </p>
+        <p className="inline"> {guestNumber} guests </p>
+      </div>
+    </div>
+  );
+}
