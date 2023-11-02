d5d00b9d76769c8d49b3a3a8ec1deafe1a1bd8e8
cpilande <120698868+cpilande@users.noreply.github.com>
2023-11-02 11:56:27 -0700

    Revert "Changes"
    This reverts commit 65123a346907876258adce9d977f66ed77c5113c.

--- src/app/auth/login/page.tsx
+++ src/app/auth/login/page.tsx
@@ -17,7 +17,6 @@ import { useGoogleLogin } from "@react-oauth/google";
 import { useState } from "react";
 import handleLogin from "../../services/auth/google";
 import styles from "./page.module.css";
-import ItineraryEvent from "@/components/Itinerary/ItineraryEvent"
 
 const PasswordInput = () => {
   const [hidden, setHidden] = useState(false);
@@ -55,8 +54,63 @@ export default function Page() {
    * handleRegister = () => {}
    */
   return (
-    <div>
-     <ItineraryEvent time = {"8:00"} location = {"Pixar Pier"} guestNumber = {"4"} />
+    <div className={styles.login}>
+      <div className={styles.header}>
+        <h1>BayView</h1>
+        <Button>Click for Guest Access</Button>
+      </div>
+      <form action={mode ? registerUser : loginUser}>
+        <a
+          onClick={(e) => {
+            e.preventDefault();
+            setMode(!mode);
+          }}
+        >
+          Dont have an account?
+        </a>
+        {mode && (
+          <>
+            <UserInput
+              name='username'
+              icon={{ icon: faIdBadge }}
+              placeholder='Username...'
+              required
+            />
+            <UserInput
+              name='first-name'
+              icon={{ icon: faPerson }}
+              placeholder='First Name...'
+              required
+            />
+            <UserInput
+              name='last-name'
+              icon={{
+                icon: faPeopleGroup
+              }}
+              placeholder='Last Name...'
+            />
+            <UserInput
+              name='phone'
+              icon={{
+                icon: faPhone
+              }}
+              placeholder='Phone Number...'
+            />
+          </>
+        )}
+        <UserInput
+          icon={{ icon: faEnvelope }}
+          placeholder='Email/Username...'
+          name='emailOrUsername'
+        ></UserInput>
+        <PasswordInput />
+        <div className={styles.buttons}>
+          <Button type='submit'>{mode ? "Register" : "Login"}</Button>
+          <Button onClick={(e) => login()}>
+            <FontAwesomeIcon icon={faGoogle} />
+          </Button>
+        </div>
+      </form>
     </div>
   );
 }
