e48b3fce928c96fc35e30d52390d282e65ff538f
rdoinhs <38991991+rdoinhs@users.noreply.github.com>
2023-11-03 16:26:38 -0700

    Add Flight Card To Homepage

--- src/app/home/[id]/page.tsx
+++ src/app/home/[id]/page.tsx
@@ -3,6 +3,7 @@
 import styles from "./page.module.css";
 import { AnimatePresence } from 'framer-motion';
 import AnimationComponent from '@/components/Animations/AnimatePresenceComponent';
+import FlightCard from "@/components/HomePage/Card/FlightCard";
 
 export default function Page() {
   return (
@@ -10,6 +11,7 @@ export default function Page() {
 		<AnimationComponent transition={{duration: 0.5}}>
 			<div className={styles.contentGrid}>
 				{/* Card Components here */}
+				<FlightCard></FlightCard>
 			</div>
 		</AnimationComponent>
 	</AnimatePresence>
