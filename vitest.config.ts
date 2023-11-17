4ff1d5b3ccca8a3a4eca1bd675559abcca840dd3
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 18:28:04 -0800

    Added dotenv loader to vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,23 +3,22 @@ import react from '@vitejs/plugin-react';
 import { resolve } from 'path';
 
 export default defineConfig({
-	plugins: [react()],
-	test: {
-		environment: 'jsdom',
-		globals: true,
-		setupFiles: ['dotenv/config'],
-		coverage: {
-			provider: 'v8',
-			all: true,
-			exclude: ['.next/*', '*.{mjs,cjs}', '*.d.*', '*.config.*', 'src/app/home/flights-testing/**/*'],
-			enabled: true,
-			reporter: ['text', 'html'],
-			include: ['src/**/*'],
-		},
-	},
-	resolve: {
-		alias: {
-			'@': resolve(__dirname, './src'),
-		},
-	},
+  plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: ['dotenv/config'],
+    coverage: {
+      provider: 'v8',
+      enabled: true,
+      reporter: ['html', 'text'],
+      include: ['src/**/*'],
+      exclude: ['src/types/**/*']
+    }
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src')
+    }
+  }
 });
