6655504046f291462258f1058cfb6fdc46c57646
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 19:24:38 -0800

    Updated Bad redirect

--- src/config/appInfo.ts
+++ src/config/appInfo.ts
@@ -1,18 +1,18 @@
 let base = {
   apiDomain: "http://localhost:3000",
-  websiteDomain: "http://localhost:3000"
+  websiteDomain: "http://localhost:3000",
 };
 
-if (process.env.ENV === "PROD") {
+if (process.env.NODE_ENV === "production") {
   base = {
     apiDomain: process.env.VERCEL_URL!,
-    websiteDomain: process.env.VERCEL_URL!
+    websiteDomain: process.env.VERCEL_URL!,
   };
 }
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
+  ...base,
   appName: "BayView",
   apiBasePath: "/api/auth",
   websiteBasePath: "/auth",
-  ...base
 };
