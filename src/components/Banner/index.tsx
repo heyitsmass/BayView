87106e71259d31e598ab1687e0c1beb5a0ea7d29
rdoinhs <38991991+rdoinhs@users.noreply.github.com>
2023-11-17 04:45:09 -0800

    Introduce banner component

---
+++ src/components/Banner/index.tsx
@@ -0,0 +1,45 @@
+import Image, { StaticImageData } from "next/image";
+import styles from "./banner.module.css";
+import { CSSProperties, ReactNode } from "react";
+
+type BannerProps = {
+    src: string;
+    height?: number;
+    width?: number;
+    blurDataURL?: string;
+    blurWidth?: number;
+    blurHeight?: number;
+    className?: string;
+    style?: CSSProperties;
+    children?: ReactNode
+};
+
+export default function Banner(props: BannerProps) {
+    const {src, className, style, children, ...spread_props } = props;
+    
+    // Combine the provided style with default styles
+    const combinedClasses = `-z-20  ${className}`;
+    const combinedStyle: CSSProperties = {
+        objectFit: 'cover',
+        objectPosition: '50% 80%',
+        ...style
+    };
+    
+	return (
+		<div className="relative w-full h-72">
+			<Image
+                src={src}
+				fill
+				priority
+				alt="Banner"
+				className={combinedClasses}
+				style={combinedStyle}
+                {...spread_props}
+			/>
+			<div className={`${styles.imageWrapper} w-full h-10 z-200`}>
+			    {/* Add content to overlay on the image here */}
+                {children}
+            </div>
+		</div>
+	);
+}
