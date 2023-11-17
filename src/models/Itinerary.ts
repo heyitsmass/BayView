16e0ae5c97227c2915765ef4bd4f1c4e136ef57e
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 18:13:49 -0800

    Updated Itinerary Model

--- src/models/Itinerary.ts
+++ src/models/Itinerary.ts
@@ -1,22 +1,36 @@
-import { IItinerary } from '@/types/Itinerary';
-import mongoose, { Schema } from 'mongoose';
-import { eventSchema } from './Event';
+import mongoose, { Schema } from "mongoose";
+import { eventSchema } from "./Event";
+import { IItinerary } from "@/types/Itinerary";
+
+export const itinerarySchema = new Schema<IItinerary>({
+  _id: { type: String, required: true, immutable: true },
+  events: [eventSchema],
+});
+
+export const ItineraryModel =
+  mongoose.models.Itinerary ||
+  mongoose.model<IItinerary>("Itinerary", itinerarySchema);
+
+/** Legacy !! Deprecated !! */
+export default ItineraryModel;
+
+/** 
 export const itinerarySchema = new Schema<IItinerary>({
   startDate: { type: Date, required: false },
-  endDate: { type: Date, required: false },
+  endDate: { type: Date, required: false},
   events: [eventSchema]
 });
 
 const Itineraries =
-  mongoose.models.Itinerary ||
-  mongoose.model<IItinerary>('Itinerary', itinerarySchema);
+    mongoose.models.Itinerary ||
+    mongoose.model<IItinerary>("Itinerary", itinerarySchema);
 
 itinerarySchema.pre('save', function (next) {
   // assuming that elements in events are sorted
   //    in chronological order
   //console.log("checking if event length is populated")
-  const { events } = this;
-  if (this.events.length > 0) {
+  const {events} = this;
+  if(this.events.length > 0){
     //console.log("events found")
     this.startDate = events.at(0)?.date;
     this.endDate = events.at(-1)?.date;
@@ -25,7 +39,6 @@ itinerarySchema.pre('save', function (next) {
   next();
 });
 
-mongoose.models?.Itinerary ||
-  mongoose.model<IItinerary>('Itinerary', itinerarySchema);
 
 export default Itineraries;
+*/
