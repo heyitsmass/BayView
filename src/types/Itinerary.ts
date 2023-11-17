e2e4d80a169a9c377909e9f23d07ebb448939420
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 18:13:49 -0800

    Updated Itinerary Model

--- src/types/Itinerary.ts
+++ src/types/Itinerary.ts
@@ -1,6 +1,18 @@
-import {Event} from './Event'; 
+import { HydratedDocument, Document, FlattenMaps } from "mongoose";
+import { Event } from "./Event";
+
 export interface IItinerary {
-  startDate?: Date;
-  endDate?: Date;
+  readonly _id: string;
   events: Event[];
 }
+
+export type ItineraryWithoutMongo = Document<
+  string,
+  {},
+  FlattenMaps<IItinerary>
+> &
+  FlattenMaps<IItinerary> &
+  Required<{
+    _id: string;
+  }>;
+export type ItineraryWithMongo = HydratedDocument<ItineraryWithoutMongo>;
