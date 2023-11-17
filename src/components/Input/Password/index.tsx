dc49c5bfc10f01b531bafea2e1086ef8a3468738
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 18:34:26 -0800

    Added password Input Compoenet

---
+++ src/components/Input/Password/index.tsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+import UserInput, { UserInputProps } from "..";
+import { faLock, faUnlock } from "@fortawesome/free-solid-svg-icons";
+
+export const PasswordInput = ({ ...props }: UserInputProps) => {
+  const [hidden, setHidden] = useState(true);
+
+  return (
+    <UserInput
+      {...props}
+      type={hidden ? "password" : "text"}
+      icon={{
+        icon: hidden ? faLock : faUnlock,
+        onClick: () => setHidden(!hidden),
+      }}
+      required
+    />
+  );
+};
