47e5a2582c14db4b62d0d97a5a68903cbc33516f
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 18:23:27 -0800

    Added Middleware for supertokens

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,6 +1,24 @@
-import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 
-const middleware = async (request: NextRequest) => {
-  
+export async function middleware() {
+  const res = NextResponse.next();
+
+  res.headers.set("Access-Control-Allow-Origin", "*");
+  res.headers.set("Access-Control-Allow-Credentials", "true");
+  /*
+  res.headers.set(
+    "Access-Control-Allow-Methods",
+    "GET,HEAD,OPTIONS,POST,PUT,PATCH,DELETE"
+  ); */
+
+  res.headers.set(
+    "Access-Control-Allow-Headers",
+    "Content-Type, anti-csrf, rid, fdi-version, authorization, st-auth-mode"
+  );
+
+  return res;
+}
+
+export const config = {
+  matcher: "/auth/:path*"
 };
-export default middleware;
