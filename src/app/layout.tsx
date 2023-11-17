e2698c5193e0408bd208dfb82cbc98518847b892
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 18:24:16 -0800

    Updated layout and provider for supertokens

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,19 +1,20 @@
 "use server";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { ReactNode } from "react";
+import { barlow } from "./fonts";
 import "./globals.css";
-import { barlow, barlowCondensed, barlowSemiCondensed } from "./fonts";
 
-import OAuthProvider from "./GoogleProvider";
-
-export default async function Layout({ children }: { children: ReactNode }) {
-	return (
-		<html className={`${barlow.variable} ${barlowCondensed.variable}  ${barlowSemiCondensed.variable}`}>
-			<body>
-				<OAuthProvider clientId={process.env.GOOGLE_CLIENT_ID!}>
-          {children}
-        </OAuthProvider>
-			</body>
-		</html>
-	);
+import { SuperTokensProvider } from "@/components/SuperTokens/SuperTokensProvider";
+export default async function Layout({
+  children,
+}: {
+  children?: ReactNode;
+}) {
+  return (
+    <html className={barlow.variable}>
+      <body>
+        <SuperTokensProvider>{children}</SuperTokensProvider>
+      </body>
+    </html>
+  );
 }
