578d588fc8f00198fbc1b653cba8895bada5d396
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 19:30:30 -0800

    Corrected App Info?

--- src/config/appInfo.ts
+++ src/config/appInfo.ts
@@ -1,17 +1,12 @@
-let base = {
-  apiDomain: "http://localhost:3000",
-  websiteDomain: "http://localhost:3000",
-};
+const URL =
+  process.env.NODE_ENV === "production"
+    ? `https://${process.env.VERCEL_URL!}`
+    : "http://localhost:3000/";
 
-if (process.env.NODE_ENV === "production") {
-  base = {
-    apiDomain: process.env.VERCEL_URL!,
-    websiteDomain: process.env.VERCEL_URL!,
-  };
-}
 export const appInfo = {
   // learn more about this on https://supertokens.com/docs/thirdpartyemailpassword/appinfo
-  ...base,
+  apiDomain: URL,
+  websiteDomain: URL,
   appName: "BayView",
   apiBasePath: "/api/auth",
   websiteBasePath: "/auth",
