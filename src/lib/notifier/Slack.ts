3e10de8164c54d6021eef1669f4f0de31f0739bc
Brandon C. <brandon.cannon88@gmail.com>
2023-11-16 18:04:39 -0800

    Relocated Notifiers into Dedicated lib location

---
+++ src/lib/notifier/Slack.ts
@@ -0,0 +1,8 @@
+import { Notifier } from ".";
+
+export class SlackNotifier implements Notifier {
+  /** Slack Notifier implementation */
+  public send_notification() {
+    /** Sends a notification to the users slack account */
+  }
+}
---
+++ src/lib/notifier/Sms.ts
@@ -0,0 +1,8 @@
+import { Notifier } from ".";
+
+export class SMSNotifier implements Notifier {
+  /** SMS Notifier implementation */
+  public send_notification() {
+    /** Sends a notification to the users phone */
+  }
+}
