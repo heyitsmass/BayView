43f95457ad4edc8ea854e6c7b986b0271384a7e6
cpilande <120698868+cpilande@users.noreply.github.com>
2023-11-20 19:53:09 -0800

    Added date checks

--- src/components/HomePage/Card/FlightCard/index.tsx
+++ src/components/HomePage/Card/FlightCard/index.tsx
@@ -11,11 +11,20 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 type ValuePiece = Date | null;
 export default function FlightCard() {
 	const [departureDate, setDepartureDate] = useState<ValuePiece | [ValuePiece, ValuePiece]>(
-    new Date()
+    null
   );
   const [returnDate, setReturnDate] = useState<ValuePiece | [ValuePiece, ValuePiece]>(
-    new Date()
+    null
   );
+   const [disabled, setDisabled] = useState(false);
+   const checkDate = (departureDate) => {
+      if (departureDate != null) {
+        setDisabled(false);
+      }
+      else {
+        setDisabled(true);
+      }
+    }
 	return (
     <Card title="Flights" subtitle="search, book, and add to itinerary">
       <form>
@@ -39,16 +48,22 @@ export default function FlightCard() {
           <ReactCalendarDemo
             label="Departure Date"
             placeholder="Date"
+            title="Departure"
             name="departure-date"
             date={departureDate}
+            minDate={new Date()}
             setDate={setDepartureDate}
+            disabled={false}
           />
           <ReactCalendarDemo
             label="Return Date"
             placeholder="Date"
+            title="Return"
             name="return-date"
             date={returnDate}
+            minDate={departureDate}
             setDate={setReturnDate}
+            disabled={departureDate === null? true: false}
           />
         </InputPair>
         <InputPair>
@@ -73,5 +88,6 @@ export default function FlightCard() {
       </form>
     </Card>
   );
+    
 
 }
--- src/components/Input/Calendar/index.tsx
+++ src/components/Input/Calendar/index.tsx
@@ -21,16 +21,21 @@ type LocationModalProps = {
   onClose: (e?: SyntheticEvent) => void;
 }; 
 
-const ReactCalendarDemo = ({ date, setDate, label, name, placeholder}) => {
-   let [isOpen, setIsOpen] = useState(false);
+const ReactCalendarDemo = ({ date, setDate, label, name, placeholder, title, minDate, disabled}) => {
+    const initialState = {
+      value: "",
+    };
 
-   function closeModal() {
-     setIsOpen(false);
-   }
+  let [isOpen, setIsOpen] = useState(false);
 
-   function openModal() {
-     setIsOpen(true);
-   }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function openModal() {
+    setIsOpen(true);
+  }
 
   const handleChange = (value) => {
     setDate(value);
@@ -40,61 +45,58 @@ const ReactCalendarDemo = ({ date, setDate, label, name, placeholder}) => {
   return (
     <>
       {label ? (
-        <label className={styles.label}>
-          {label}
-        </label>
+        <label className={styles.label}>{label}</label>
       ) : (
         <div className="w-full h-7"></div>
       )}
-    
-      <div className={styles.inputWrapper}>
-        <FontAwesomeIcon
-          className={styles.icon}
-          icon={faCalendarDays}
-          onClick={openModal}
-        />
-        <input
-          className={styles.input}
-          value={date.toLocaleDateString()} required placeholder = {placeholder}
-        ></input>
-      </div>
 
-      <Transition appear show={isOpen} as={Fragment}>
-        <Dialog open={isOpen} onClose={closeModal}>
-          <div className="fixed inset-0 flex w-screen items-center ml-3 ">
-            <Transition.Child
-              as={Fragment}
-              enter="ease-out duration-300"
-              enterFrom="opacity-0 scale-95"
-              enterTo="opacity-100 scale-100"
-              leave="ease-in duration-200"
-              leaveFrom="opacity-100 scale-100"
-              leaveTo="opacity-0 scale-95"
-            >
-              <Dialog.Panel className="w-full max-w-xs max-h-min rounded bg-white pl-5 pb-5 pr-5 border-4 border-gray-200">
-                <div className="text-2xl font-bold text-center pb-3 pt-2">
-                  <Dialog.Title>Select Date</Dialog.Title>
-                </div>
-                <div>
-                  <p
-                    suppressHydrationWarning={true}
-                    className="text-center pb-5"
-                  >
-                    {" "}
-                    {date.toLocaleDateString()}
-                  </p>
-                </div>
-                <Calendar
-                  /*className={`${styles.reactCalendar}`}*/
-                  minDate={new Date()}
-                  value={date}
-                  onChange={handleChange}
-                />
-              </Dialog.Panel>
-            </Transition.Child>
-          </div>
-        </Dialog>
-      </Transition>
+      <div className="relative">
+        <div className={styles.inputWrapper}>
+          <FontAwesomeIcon
+            className={styles.icon}
+            icon={faCalendarDays}
+            onClick={disabled === true ? () => {}: openModal}
+          />
+          <input
+            className={styles.input}
+            disabled={disabled}
+            placeholder = {date === null ? placeholder: ""}
+            required
+            value={date === null ? "" : date.toLocaleDateString()}
+          ></input>
+        </div>
+
+        <Transition appear show={isOpen} as={Fragment}>
+          <Dialog open={isOpen} onClose={closeModal}>
+            <div className="fixed inset-0 bg-black/30" aria-hidden="true" />
+
+            <div className="fixed inset-0 flex w-screen items-center justify-center">
+              <Transition.Child
+                as={Fragment}
+                enter="ease-out duration-300"
+                enterFrom="opacity-0 scale-95"
+                enterTo="opacity-100 scale-100"
+                leave="ease-in duration-200"
+                leaveFrom="opacity-100 scale-100"
+                leaveTo="opacity-0 scale-95"
+              >
+                <Dialog.Panel className="w-full max-w-xs max-h-min rounded bg-white pl-5 pb-5 pr-5 border-4 border-gray-200">
+                  <div className="text-2xl font-bold text-center pb-3 pt-2">
+                    <Dialog.Title>Select {title} Date</Dialog.Title>
+                  </div>
+                  <div></div>
+                  <Calendar
+                    /*className={`${styles.reactCalendar}`}*/
+                    minDate={minDate}
+                    value={date}
+                    onChange={handleChange}
+                  />
+                </Dialog.Panel>
+              </Transition.Child>
+            </div>
+          </Dialog>
+        </Transition>
+      </div>
     </>
   );
 };
