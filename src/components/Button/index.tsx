c58cccd8727056fcce3d22f3ccd6af452b5f2ba0
rdoinhs <38991991+rdoinhs@users.noreply.github.com>
2023-11-24 23:18:03 -0800

    Memoize FAIcon for Default Button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,9 @@
 'use client';
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode, useState } from 'react';
 import styles from './button.module.css';
-import { FontAwesomeIcon} from '@fortawesome/react-fontawesome';
+import { FontAwesomeIcon, FontAwesomeIconProps} from '@fortawesome/react-fontawesome';
 import { IconDefinition} from "@fortawesome/fontawesome-svg-core";
+import React from 'react';
 
 type ButtonProps = ({
 } & DetailedHTMLProps<ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> & {
@@ -17,12 +18,16 @@ export default function Button({
   ...props
 }: ButtonProps) {
   const classNames = [props.className, props.variant ? styles[props.variant] : styles.primary, styles.button].join(' ');
-
+  const FAIcon = React.memo(({ ...props }: FontAwesomeIconProps) => {
+    return <FontAwesomeIcon {...props} />;
+  });
+  FAIcon.displayName = 'FAIcon';
+  
   return (
   <button {...props} {... props.pending && ({"disabled": true, "aria-disabled": true})} className={`${classNames}`}>
     	{ props.icon && 
         <div className={styles.inlineIcon}>
-				  <FontAwesomeIcon className="text-white w-3" size="lg" icon={props.icon}/>
+				  <FAIcon className="text-white w-3" size="lg" icon={props.icon}/>
         </div> 
       }
       <div className='w-full justify-self-center'>{props.children}</div>
