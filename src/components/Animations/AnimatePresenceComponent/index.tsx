241753eaa3f3e5ed1c89a9abf3bd0f6b4f73e0b9
rdoinhs <38991991+rdoinhs@users.noreply.github.com>
2023-11-24 23:17:12 -0800

    Refine/Add Anim. Helpers To Anim. Presence Comp.

--- src/components/Animations/AnimatePresenceComponent/index.tsx
+++ src/components/Animations/AnimatePresenceComponent/index.tsx
@@ -1,5 +1,6 @@
 'use client';
-import { animations, cubicBezier, motion, MotionProps } from 'framer-motion';
+import { animations, cubicBezier, easeIn, easeInOut, motion, MotionProps } from 'framer-motion';
+import { ReactNode, ReactNodeArray } from 'react';
 
 type AnimationProps = {
 	type?: keyof typeof animations;
@@ -8,6 +9,15 @@ type AnimationProps = {
 type AnimationTypes = {
 	[key: string]: any;
 };
+
+type BlurOpacityAnimationProps = {
+	children: ReactNode;
+	transitionOn: boolean;
+	className?: string;
+};
+
+const easeOutExpo = cubicBezier(0.16, 1, 0.3, 1);
+export const easeOutExpoTransition = { type: 'tween', ease: easeOutExpo, duration: 0.5 };
 
 export const blurFadeVariant = {
 	closed: { opacity: 0, maxHeight: 0, overflow: 'hidden', filter: 'blur(10px)' },
@@ -20,21 +30,55 @@ export const titleTransitionVariant = {
 	exit: { opacity: 0, x: 20, filter: 'blur(10px)' },
 };
 
+export const pendingTransitionVariant = {
+	blur: { opacity: 0.3, filter: 'blur(3px)' },
+	show: { opacity: 1, filter: 'blur(0px)' },
+};
+
+export const pendingTransitionParams = {
+	opacity: {ease: easeInOut, duration: 1},
+	filter: {ease: easeInOut, duration: 1},
+};
+
 export const titleTransitionParams = {
 	type: 'tween',
 	ease: 'easeInOut',
 	duration: 0.5,
 };
 
-const easeOutExpo = cubicBezier(0.16, 1, 0.3, 1);
-export const easeOutExpoTransition = { type: 'tween', ease: easeOutExpo, duration: 0.5 };
+export const blurVariant = {
+	blur: { filter: 'blur(2px)' },
+	show: { filter: 'blur(0px)' },
+};
 
-export default function AnimationComponent({ ...props }: AnimationProps) {
+export const blurTransitionParams = {
+	type: 'tween',
+	ease: 'easeInOut',
+	duration: 0.2,
+};
+
+export const BlurOpacityAnimation = ({ ...props }: BlurOpacityAnimationProps) => {
+	return (
+		<motion.div
+			animate={props.transitionOn ? 'blur' : 'show'}
+			transition={pendingTransitionParams}
+			variants={pendingTransitionVariant}
+			className={`${props.className} ${props.transitionOn && 'pointer-events-none cursor-not-allowed'}`} // Disable mouse events on key
+		>	
+			{/* Disable any enclosing form fields */}
+			<fieldset disabled={props.transitionOn} aria-disabled={props.transitionOn}>
+				{props.children}
+			</fieldset>
+		</motion.div>
+	);
+};
+
+export function AnimationComponent({ ...props }: AnimationProps) {
 	const defaultTransition = {
 		type: 'tween',
 		ease: 'easeInOut',
-		duration: 0.2
-	}
+		duration: 0.2,
+	};
 
 	const animations: AnimationTypes = {
 		fadeSlideBlur: {
@@ -49,7 +93,7 @@ export default function AnimationComponent({ ...props }: AnimationProps) {
 	const animationVariant = animations.fadeSlideBlur;
 
 	// Merge custom transition with defaultTransition
-	const appliedTransition = { ...defaultTransition, ...props.transition};
+	const appliedTransition = { ...defaultTransition, ...props.transition };
 
 	return (
 		<motion.div
