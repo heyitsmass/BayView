e77e2134205cc3ec6b5015df0bf09f3c2e8c5eb5
Brandon C. <brandon.cannon88@gmail.com>
2023-11-24 16:33:09 -0800

    Added partyMember data to Itinerary

--- src/types/Itinerary.ts
+++ src/types/Itinerary.ts
@@ -1,10 +1,20 @@
 import { HydratedDocument, Document, FlattenMaps } from "mongoose";
 import { Event } from "./Event";
+import { PartyMember } from "@/components/Itinerary/Party";
+import { DisplayData } from ".";
+import { FlattenedEvent } from "@/app/home/itinerary/page";
 
 export interface IItinerary {
   readonly _id: string;
   events: Event[];
+  party: PartyMember[];
 }
+
+export type FlattenedItinerary = {
+  readonly _id: string;
+  events: FlattenedEvent[];
+  party: PartyMember[];
+};
 
 export type ItineraryWithoutMongo = Document<
   string,
@@ -15,4 +25,5 @@ export type ItineraryWithoutMongo = Document<
   Required<{
     _id: string;
   }>;
+
 export type ItineraryWithMongo = HydratedDocument<ItineraryWithoutMongo>;
