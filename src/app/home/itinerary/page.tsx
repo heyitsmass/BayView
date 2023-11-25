842903f3f2b20b3f634c3ace4286f5811c8977f9
Brandon C. <brandon.cannon88@gmail.com>
2023-11-24 19:25:51 -0800

    Updated page to support modifications

--- src/app/home/itinerary/page.tsx
+++ src/app/home/itinerary/page.tsx
@@ -8,6 +8,8 @@ import { Event, EventTypes } from "@/types/Event";
 import { useState } from "react";
 import styles from "./itinerary.module.css";
 
+import random from "@/lib/random";
+
 export type FlattenedEvent = Event &
   DisplayData & {
     _id: string;
@@ -21,18 +23,21 @@ export default function Page() {
 
   const [currentEvent, setEvent] = useState(events[0]);
 
-  console.log(currentEvent);
-
   const handleClick = (index: number) => {
     if (index >= events.length) return;
 
     setEvent(events[index]);
   };
 
+  const members = random.member({
+    min: 1,
+    max: 4,
+  });
+
   return (
     <div className="flex h-full justify-center items-center overflow-y-scroll ">
       <div className={styles.content + "  w-full flex gap-8"}>
-        <CurrentEvent {...currentEvent} />
+        <CurrentEvent {...currentEvent} members={members} />
 
         <EventList events={events} handleClick={handleClick}></EventList>
       </div>
