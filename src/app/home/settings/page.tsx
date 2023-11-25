c6bc80e6d49d669715d243b8b7717c235d5abc23
Brandon C. <brandon.cannon88@gmail.com>
2023-11-24 16:55:09 -0800

    Added sample notification request button

--- src/app/home/settings/page.tsx
+++ src/app/home/settings/page.tsx
@@ -1,9 +1,47 @@
 "use client";
 import { useHomepage } from "@/context";
+import { HandleCall } from "@/lib/notifier/Handler";
 
 export default function Page() {
   const ctx = useHomepage();
 
   const { user } = ctx!;
-  return <></>;
+
+  /** delay in seconds */
+
+  const { _id } = user;
+
+  return (
+    <>
+      <div>
+        <h2>Get notified!</h2>
+        <div>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={async (e) => {
+              e.preventDefault();
+              await HandleCall({
+                _id,
+                type: "Email",
+                delay: 10,
+                mode: "create",
+              });
+            }}
+          >
+            {" "}
+            Get Notified in 10 seconds!{" "}
+          </button>
+          <button
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            onClick={async (e) => {
+              e.preventDefault();
+              await HandleCall({ _id, type: "Email", mode: "cancel" });
+            }}
+          >
+            Cancel the timer!
+          </button>
+        </div>
+      </div>
+    </>
+  );
 }
