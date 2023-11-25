c040fd4d2a1491c612b26aca1fb88939799e1978
Brandon C. <brandon.cannon88@gmail.com>
2023-11-24 16:53:28 -0800

    Updated policies to be scrollable

--- src/app/home/policies/layout.tsx
+++ src/app/home/policies/layout.tsx
@@ -1,9 +1,13 @@
 import { ReactNode } from "react";
+import styles from "./layout.module.css";
 
 export default function Layout({ children }: { children: ReactNode }) {
   return (
     <div
-      className="border m-20 rounded-2xl bg-zinc-800 border-zinc-700 shadow-2xl p-4"
+      className={
+        styles.layout +
+        " border m-20 rounded-2xl bg-zinc-800 border-zinc-700 shadow-2xl p-4"
+      }
       style={{
         height: "min-content",
       }}
