7892f634685a247efa396e7ec5c2b73a1bf9e922
Brandon C. <brandon.cannon88@gmail.com>
2023-11-24 21:20:43 -0800

    Refined Actions

--- src/components/Itinerary/Actions.tsx
+++ src/components/Itinerary/Actions.tsx
@@ -1,7 +1,7 @@
 "use client";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import styles from "./actions.module.css";
+import { faDiscord, faFacebook } from "@fortawesome/free-brands-svg-icons";
 import {
+  IconDefinition,
   faArrowsRotate,
   faBars,
   faCar,
@@ -14,10 +14,11 @@ import {
   faTrashCan,
   faXmark,
 } from "@fortawesome/free-solid-svg-icons";
-import { faDiscord, faFacebook } from "@fortawesome/free-brands-svg-icons";
-import { SyntheticEvent, useEffect, useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { ReactNode, SyntheticEvent, useState } from "react";
+import styles from "./actions.module.css";
 
-type ActionProps = {
+type ActionsProps = {
   notify: {
     email: boolean;
     phone: boolean;
@@ -26,110 +27,78 @@ type ActionProps = {
   };
 };
 
-export const Actions = ({ notify }: ActionProps) => {
-  const [states, setStates] = useState({
-    notify: false,
-    map: false,
-    share: false,
-    updates: false,
-  });
-
-  const handleClick = (e: SyntheticEvent, state: keyof typeof states) => {
-    e.preventDefault();
-    console.log(state, states[state]);
-    return setStates((states) => {
-      return {
-        ...states,
-        [state]: !states[state],
-      };
-    });
-  };
-
+export const Actions = ({ notify }: ActionsProps) => {
   return (
     <div className={styles.actions}>
-      <div className="flex flex-col items-center w-1/6">
-        <label className={styles.lg_label}>Get Directions!</label>
-        <label className={styles.sm_label}>Map!</label>
-        <span
-          className={[
-            styles.icons,
-            states.map ? styles.open : styles.closed,
-          ].join(" ")}
-        >
-          <span className={styles.bars}>
-            <FontAwesomeIcon
-              icon={states.map ? faXmark : faBars}
-              onClick={(e) => handleClick(e, "map")}
-            />
-          </span>
-          <FontAwesomeIcon icon={faCar} />
-          <FontAwesomeIcon icon={faMapPin} />
-        </span>
-      </div>
+      <Action largeLabel="Get Directions!" smallLabel="Map">
+        {[faCar, faMapPin]}
+      </Action>
 
-      <div className="flex flex-col items-center w-1/4">
-        <label className={styles.lg_label}>Get Notified!</label>
-        <label className={styles.sm_label}>Notify!</label>
-        <span
-          className={[
-            styles.icons,
-            states.notify ? styles.open : styles.closed,
-          ].join(" ")}
-        >
-          <span className={styles.bars}>
-            <FontAwesomeIcon
-              icon={states.notify ? faXmark : faBars}
-              onClick={(e) => handleClick(e, "notify")}
-            />
-          </span>
+      <Action largeLabel="Get Notified!" smallLabel="Notify!">
+        <>
           {notify.email && <FontAwesomeIcon icon={faEnvelope} />}
           {notify.phone && <FontAwesomeIcon icon={faPhone} />}
           {notify.discord && <FontAwesomeIcon icon={faDiscord} />}
           {notify.facebook && <FontAwesomeIcon icon={faFacebook} />}
-        </span>
-      </div>
-      <div className="flex flex-col items-center w-1/6">
-        <label className={styles.lg_label}>Share Your Trip!</label>
-        <label className={styles.sm_label}>Share!</label>
+        </>
+      </Action>
 
-        <span
-          className={[
-            styles.icons,
-            states.share ? styles.open : styles.closed,
-          ].join(" ")}
-        >
-          <span className={styles.bars}>
-            <FontAwesomeIcon
-              icon={states.share ? faXmark : faBars}
-              onClick={(e) => handleClick(e, "share")}
-            />
-          </span>
-          <FontAwesomeIcon icon={faRss} />
-          <FontAwesomeIcon icon={faShareNodes} />
-          <FontAwesomeIcon icon={faShare} />
+      <Action largeLabel="Share Your Trip!" smallLabel="Share!">
+        {[faRss, faShareNodes, faShare]}
+      </Action>
+
+      <Action
+        largeLabel="Quick Updates"
+        smallLabel="Updates"
+        className="min-w-max"
+      >
+        {[faArrowsRotate, faTrashCan]}
+      </Action>
+    </div>
+  );
+};
+
+type ActionProps = {
+  largeLabel: string;
+  smallLabel: string;
+  children: IconDefinition[] | ReactNode;
+  className?: string;
+};
+
+const Action = ({ ...props }: ActionProps) => {
+  const { largeLabel, smallLabel, className, children } = props;
+
+  const [isOpen, setOpen] = useState(false);
+
+  const handleClick = (e: SyntheticEvent) => {
+    e.preventDefault();
+    return setOpen(!isOpen);
+  };
+
+  const display =
+    children instanceof Array
+      ? children.map((icon, i) => <FontAwesomeIcon key={i} icon={icon} />)
+      : children;
+
+  return (
+    <div className="flex flex-col items-center w-1/6">
+      <label className={[styles.lg_label, className].join(" ")}>
+        {largeLabel}
+      </label>
+      <label className={styles.sm_label}>{smallLabel}</label>
+      <span
+        className={`${styles.icons} ${
+          isOpen ? styles.open : styles.closed
+        }`}
+      >
+        <span className={styles.bars}>
+          <FontAwesomeIcon
+            icon={isOpen ? faXmark : faBars}
+            onClick={handleClick}
+          />
         </span>
-      </div>
-      <div className="flex flex-col items-center w-1/6">
-        <label className={styles.lg_label + " min-w-max"}>
-          Quick Updates
-        </label>
-        <label className={styles.sm_label}>Updates</label>
-        <span
-          className={[
-            styles.icons,
-            states.updates ? styles.open : styles.closed,
-          ].join(" ")}
-        >
-          <span className={styles.bars}>
-            <FontAwesomeIcon
-              icon={states.updates ? faXmark : faBars}
-              onClick={(e) => handleClick(e, "updates")}
-            />
-          </span>
-          <FontAwesomeIcon icon={faArrowsRotate} />
-          <FontAwesomeIcon icon={faTrashCan} />
-        </span>
-      </div>
+        {display}
+      </span>
     </div>
   );
 };
