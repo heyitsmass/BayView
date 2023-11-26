b33938a9640cf607757f2c1ba718463f7b72b93b
Brandon C. <brandon.cannon88@gmail.com>
2023-11-26 15:17:52 -0800

    Added backend event deletion handler

--- src/handlers/Itinerary/actions/helpers/deleteEvent.ts
+++ src/handlers/Itinerary/actions/helpers/deleteEvent.ts
@@ -1,7 +1,35 @@
 "use server";
-export type DeleteEventPayload = {};
+
+import ItineraryModel from "@/models/Itinerary";
+import { IItinerary } from "@/types/Itinerary";
+import { HydratedDocument } from "mongoose";
+
+export type DeleteEventPayload = {
+  _id: string;
+  event_id: string;
+};
 
 /** Remove the event from the itinerary in the database */
-const handleDeleteEvent = async ({ ...props }: DeleteEventPayload) => {};
+const handleDeleteEvent = async ({
+  ...props
+}: DeleteEventPayload): Promise<HydratedDocument<IItinerary> | null> => {
+  const { _id, event_id } = props;
+
+  return ItineraryModel.findOneAndUpdate(
+    {
+      _id,
+    },
+    {
+      $pull: {
+        events: {
+          _id: event_id,
+        },
+      },
+    },
+    {
+      new: true,
+    }
+  );
+};
 
 export { handleDeleteEvent };
