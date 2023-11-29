82faa9ff3cb301f04c6d77dfb7cea457882e559a
Brandon C. <brandon.cannon88@gmail.com>
2023-11-29 09:57:40 -0800

    Corrected Actions Dialog

--- src/components/Dialog/Actions.tsx
+++ src/components/Dialog/Actions.tsx
@@ -1,23 +1,40 @@
-import { useCurrentEvent } from "@/hooks";
-import { ContainedDialog, CustomDialogProps } from ".";
+import { useCurrentEvent, useOpen } from "@/hooks";
+import { ContainedDialog, CustomDialogProps, PrebuiltDialog } from ".";
 import { ActionMethods } from "../Itinerary/Actions";
 import dialogs from "../Itinerary/Dialogs";
+import { PropsWithChildren, cloneElement } from "react";
 
-type ActionDialogProps<T extends ActionMethods> = Omit<
-  CustomDialogProps<{
-    btn: JSX.Element;
-    type: T;
-  }>,
-  "title" | "description"
->;
+type ActionDialogProps<T extends ActionMethods> = {
+  btn: JSX.Element;
+  type: T;
+};
 
 export function ActionsDialog<T extends ActionMethods>({
   type,
   ...props
 }: ActionDialogProps<T>) {
   const currEvent = useCurrentEvent();
+  const [isOpen, open, close] = useOpen();
 
   if (!currEvent) throw new Error("No current event");
 
-  return <ContainedDialog {...props} {...dialogs[type]} />;
+  const { title, description, Component } = dialogs[type];
+
+  return (
+    <>
+      {cloneElement(props.btn, {
+        onClick: open,
+      })}
+      {isOpen && (
+        <PrebuiltDialog
+          title={title}
+          description={description}
+          open={isOpen}
+          onClose={close}
+        >
+          <Component next={close} />
+        </PrebuiltDialog>
+      )}
+    </>
+  );
 }
