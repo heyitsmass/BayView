69b366d326974e51a010b03e52edfc7be8637130
rdoinhs <38991991+rdoinhs@users.noreply.github.com>
2023-12-01 14:35:08 -0800

    Fix Blur

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -56,21 +56,23 @@ export default function Page() {
   const enhancedCards = enhanceCards(initialCards);
 
   return (
-    <AnimationComponent transition={{ duration: 0.5 }}>
-      <div className={styles.contentGrid}>
-        <div className="w-[600px] col-span-full">
-          <ItineraryTitle></ItineraryTitle>
-        </div>
-        {cardOrder.map((cardId) => (
-          <motion.div layout key={cardId}>
-            <CardCollapseContext.Provider
-              value={{ onCollapse: () => handleCollapse(cardId) }}
-            >
-              {enhancedCards.find((card) => card.props.cardId === cardId)}
-            </CardCollapseContext.Provider>
-          </motion.div>
-        ))}
-      </div>
-    </AnimationComponent>
-  );
+		<>
+			<div className={styles.contentGrid + " !pb-0 !pt-16"}>
+				<div className="w-[600px] col-span-full">
+					<ItineraryTitle></ItineraryTitle>
+				</div>
+			</div>
+			<AnimationComponent transition={{ duration: 0.5 }}>
+				<div className={styles.contentGrid}>
+					{cardOrder.map((cardId) => (
+						<motion.div layout key={cardId}>
+							<CardCollapseContext.Provider value={{ onCollapse: () => handleCollapse(cardId) }}>
+								{enhancedCards.find((card) => card.props.cardId === cardId)}
+							</CardCollapseContext.Provider>
+						</motion.div>
+					))}
+				</div>
+			</AnimationComponent>
+		</>
+	);
 }
