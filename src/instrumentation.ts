19cea2864db4b35fc3cbc4a28ce9790208dd2344
Brandon C. <brandon.cannon88@gmail.com>
2023-12-01 07:47:10 -0800

    Hot patch

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -1,5 +1,46 @@
-export async function register() {
-  const { connectToDB } = await import('./utils/connectToDB');
+import mongoose from "mongoose";
 
-  await connectToDB();
+const envTypes = ["production", "development", "preview"] as const;
+
+const dbNames = ["bayview", "bayview-dev", "bayview-dev"] as const;
+
+type NEXT_VERCEL_ENV = (typeof envTypes)[number];
+
+type MONGODB_COLLECTIONS = (typeof dbNames)[number];
+
+export async function register() {
+  const dbMap = {
+    production: "bayview",
+    development: "bayview-dev",
+    preview: "bayview-dev"
+  } as Record<NEXT_VERCEL_ENV, MONGODB_COLLECTIONS>;
+
+  const ENV = process.env.NEXT_PUBLIC_VERCEL_ENV || "production";
+
+  const MONGODB_URI =
+    process.env.MONGODB_URI || "mongodb://localhost:27017";
+
+  try {
+    console.log(
+      `Connecting to ${
+        ENV === "production" ? "production" : "development"
+      } database...`
+    );
+
+    await mongoose.connect(
+      `${MONGODB_URI}/${dbMap[ENV]}?retryWrites=true&w=majority`
+    );
+
+    const { name, port } = mongoose.connection;
+
+    if (ENV !== "production") {
+      console.log("\n*********** MongoDB ***********\n");
+      console.log(`  Name: ${name}`);
+      console.log(`  Port: ${port}`);
+      console.log("\n********** Connected **********\n");
+    }
+  } catch (err) {
+    const { message } = err as Error;
+    console.log(message);
+  }
 }
