cd985d3cae22faf2f518a0fe801defc9d6a1eb31
Brandon C. <brandon.cannon88@gmail.com>
2023-12-04 19:32:21 -0800

    Added Handlers for Party Member Functionality

--- src/app/Handler.ts
+++ src/app/Handler.ts
@@ -1,11 +1,14 @@
 "use server";
 
+import { DisplayableEvent } from "@/components/HomePage/EventFinder";
 import { HomepageAction } from "@/context";
 import { handleItineraryActionRequest } from "@/handlers/Itinerary/actions";
 import { modelTypes } from "@/lib/constants";
-import { DisplayableEvent } from "@/lib/random/handler";
-import ItineraryModel from "@/models/Itinerary";
 
+import ItineraryModel from "@/models/Itinerary";
+import { TUserMetadata } from "@/types/User";
+import { getSSRSession } from "@/utils/session/getSSRSession";
+import UserMetadata from "supertokens-node/recipe/usermetadata";
 type HandlerResponse<T = any, S = number> = {
   statusCode: S;
   body: T;
@@ -57,6 +60,26 @@ export default async function Handler(
       case "event":
         body = await handleEventRequest(_id, action);
         break;
+
+      case "event_party":
+        switch (action.mode) {
+          case "add":
+            body = await handleAddPartyMemberToEvent(_id, action.payload);
+            break;
+          case "remove":
+            body = await handleRemovePartyMemberFromEvent(
+              _id,
+              action.payload
+            );
+            break;
+        }
+
+      case "party_member":
+        switch (action.mode) {
+          case "delete":
+            body = await handleDeletePartyMember(_id, action.payload);
+            break;
+        }
     }
 
     res = {
@@ -76,7 +99,110 @@ export default async function Handler(
   return res;
 }
 
-const handleEventRequest = async (_id: string, action: HomepageAction) => {
+const handleDeletePartyMember = async (
+  _id: string,
+  payload: {
+    member_id: string;
+  }
+) => {
+  const res = await ItineraryModel.updateOne(
+    //delete the id from any events in mongo
+    {
+      _id
+    },
+    {
+      $pull: {
+        events: {
+          party: payload.member_id
+        }
+      }
+    }
+  );
+
+  const { session } = await getSSRSession();
+
+  if (!session) throw new Error("Session not found");
+
+  const userId = session?.getUserId();
+
+  const { metadata } = (await UserMetadata.getUserMetadata(userId)) as {
+    metadata: TUserMetadata;
+  };
+
+  if (
+    metadata.party.members.length <= 0 ||
+    !metadata.party.members.find(
+      (member) => member._id === payload.member_id
+    )
+  ) {
+    return {
+      message: "Party member does not exist in this itinerary"
+    };
+  }
+
+  await UserMetadata.updateUserMetadata(userId, {
+    ...metadata,
+    party: {
+      ...metadata.party,
+      members: metadata.party.members.filter(
+        (member) => member._id !== payload.member_id
+      )
+    }
+  });
+
+  return {
+    message: "Party member deleted from itinerary"
+  };
+};
+const handleRemovePartyMemberFromEvent = async (
+  _id: string,
+  payload: {
+    event_id: string;
+    member_id: string;
+  }
+) => {
+  const res = await ItineraryModel.updateOne(
+    {
+      _id,
+      "events._id": payload.event_id
+    },
+    {
+      $pull: {
+        "events.$.party": payload.member_id
+      }
+    }
+  );
+
+  return {
+    message: "Party member removed from event"
+  };
+};
+
+const handleAddPartyMemberToEvent = async (
+  _id: string,
+  payload: {
+    event_id: string;
+    member_id: string;
+  }
+) => {
+  const res = await ItineraryModel.updateOne(
+    {
+      _id,
+      "events._id": payload.event_id
+    },
+    {
+      $push: {
+        "events.$.party": payload.member_id
+      }
+    }
+  );
+
+  return {
+    message: "Party member added to event"
+  };
+};
+
+const handleEventRequest = async (_id: string, action) => {
   switch (action.mode) {
     case "add":
       return handleAddEventRequest(_id, action.payload);
