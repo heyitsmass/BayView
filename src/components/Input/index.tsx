022bc75a25ed4c686c01a37c4b4cd05f8767a592
Brandon C. <brandon.cannon88@gmail.com>
2023-12-04 19:42:56 -0800

    Added Checkbox button

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,18 +1,12 @@
 "use client";
-import {
-  DetailedHTMLProps,
-  InputHTMLAttributes,
-  forwardRef,
-  useCallback,
-  useState, SyntheticEvent
-} from "react";
+import { DetailedHTMLProps, InputHTMLAttributes, forwardRef } from "react";
 
+import { IconProp } from "@fortawesome/fontawesome-svg-core";
+import { faSquare, faSquareCheck } from "@fortawesome/free-solid-svg-icons";
 import {
   FontAwesomeIcon,
   FontAwesomeIconProps
 } from "@fortawesome/react-fontawesome";
-import { IconProp } from "@fortawesome/fontawesome-svg-core";
-import { IconDefinition} from "@fortawesome/fontawesome-svg-core";
 import styles from "./input.module.css";
 
 type IconProps = Omit<FontAwesomeIconProps, "icon"> & {
@@ -21,52 +15,71 @@ type IconProps = Omit<FontAwesomeIconProps, "icon"> & {
 };
 
 export type UserInputProps = {
+  inputClassName?: string;
   icon?: IconProps;
   label?: string;
+  isselected?: string;
 } & DetailedHTMLProps<
   InputHTMLAttributes<HTMLInputElement>,
   HTMLInputElement
 >;
 
-function useInputValidation(initialValue: string = "") {
-	const [input, setInput] = useState(initialValue);
+const UserInput = forwardRef<HTMLInputElement, UserInputProps>(
+  (props, ref) => {
+    const { icon, label, id, children, inputClassName } = props;
 
-	const validateInput = (value: string): void => {
-		// Validation logic here
-		setInput(value);
-	};
+    if (props.type === "checkbox") {
+      return (
+        <div
+          className={
+            props.className +
+            " w-max p-4 h-min flex items-center justify-around gap-4 rounded-full font-semibold py-2 bg-red-600"
+          }
+        >
+          <label htmlFor="$checkbox">{props.label}</label>
 
-	return [input, validateInput] as const;
-}
+          <FontAwesomeIcon
+            id="$checkbox"
+            onClick={icon?.onClick}
+            icon={props.isselected === "true" ? faSquareCheck : faSquare}
+            className="hover:cursor-pointer hover:opacity-90"
+          />
+        </div>
+      );
+    }
 
-const UserInput = forwardRef<HTMLInputElement, UserInputProps>((props, ref) => {
-  const { icon, label, id, children, className } = props;
-
-  const [input, validateInput] = useInputValidation();
-  const onKeyUp = useCallback(
-    (event: SyntheticEvent) =>
-      validateInput((event.currentTarget as HTMLInputElement).value),
-    [validateInput]
-  );
-
-  const style = [styles.input, className].join(" ");
-
-  return (
-    <div className={className}>
-      {label ? (
-        <label className={styles.label} htmlFor={id}>
-          {label}
-        </label>
-      ) : (
-        <div className="w-full h-7"></div>
-      )}
-      <div className={styles.inputWrapper}>
-        {icon && <FontAwesomeIcon className={styles.icon} {...icon} />}
-        <input {...props} ref={ref} className={style} />
-        {children}
+    return (
+      <div>
+        {label ? (
+          <label className={styles.label} htmlFor={id}>
+            {label}
+            <small className="text-lg font-semibold">
+              <b className={props.required ? "!text-red-400" : ""}>
+                {" "}
+                {props.disabled ? "" : props.required ? "*" : ""}{" "}
+              </b>
+            </small>
+          </label>
+        ) : (
+          <div className="w-full h-7"></div>
+        )}
+        <div className={styles.inputWrapper}>
+          {icon && <FontAwesomeIcon className={styles.icon} {...icon} />}
+          <input
+            {...props}
+            ref={ref}
+            className={[props.className, styles.input].join(" ")}
+            name={
+              !props.name
+                ? props.label?.replaceAll(" ", "_").toLowerCase()
+                : props.name
+            }
+          />
+          {children}
+        </div>
       </div>
-    </div>
-  );
-});
+    );
+  }
+);
 UserInput.displayName = "UserInput";
 export default UserInput;
