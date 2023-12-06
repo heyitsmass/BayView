2faeff1d8f9c4d1a6039bffae4a7ee5a498ea394
Brandon C. <brandon.cannon88@gmail.com>
2023-12-06 11:00:54 -0800

    Final instrumentation fix

--- src/instrumentation.ts
+++ src/instrumentation.ts
@@ -7,33 +7,35 @@ type NEXT_VERCEL_ENV = (typeof envTypes)[number];
 type MONGODB_COLLECTIONS = (typeof dbNames)[number];
 
 export async function register() {
-  const mongoose = await import("mongoose");
-  const dbMap = {
-    production: "bayview",
-    development: "bayview-dev",
-    preview: "bayview-dev"
-  } as Record<NEXT_VERCEL_ENV, MONGODB_COLLECTIONS>;
+  if (process.env.NEXT_RUNTIME === "nodejs") {
+    const mongoose = await import("mongoose");
+    const dbMap = {
+      production: "bayview",
+      development: "bayview-dev",
+      preview: "bayview-dev"
+    } as Record<NEXT_VERCEL_ENV, MONGODB_COLLECTIONS>;
 
-  const ENV = process.env.NEXT_PUBLIC_VERCEL_ENV || "development";
+    const ENV = process.env.NEXT_PUBLIC_VERCEL_ENV || "development";
 
-  const MONGODB_URI =
-    process.env.MONGODB_URI || "mongodb://localhost:27017";
+    const MONGODB_URI =
+      process.env.MONGODB_URI || "mongodb://localhost:27017";
 
-  if (!mongoose.connection?.readyState) {
-    console.log(`Connecting to ${ENV} database...`);
+    if (!mongoose.connection?.readyState) {
+      console.log(`Connecting to ${ENV} database...`);
 
-    await mongoose.connect(
-      `${MONGODB_URI}/${dbMap[ENV]}?retryWrites=true&w=majority`,
-      {}
-    );
+      await mongoose.connect(
+        `${MONGODB_URI}/${dbMap[ENV]}?retryWrites=true&w=majority`,
+        {}
+      );
 
-    const { name, port } = mongoose.connection;
+      const { name, port } = mongoose.connection;
 
-    if (ENV !== "production") {
-      console.log("\n*********** MongoDB ***********\n");
-      console.log(`  Name: ${name}`);
-      console.log(`  Port: ${port}`);
-      console.log("\n********** Connected **********\n");
+      if (ENV !== "production") {
+        console.log("\n*********** MongoDB ***********\n");
+        console.log(`  Name: ${name}`);
+        console.log(`  Port: ${port}`);
+        console.log("\n********** Connected **********\n");
+      }
     }
   }
 }
